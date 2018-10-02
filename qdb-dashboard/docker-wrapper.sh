10922ad63a85a73823d9defa6ee45c0f6018ecbd
Leon Mergen <leon@solatis.com>
2018-10-02 16:18:58 +0200

    Allow for configurable allowed_host and qdb_uri

---
+++ qdb-dashboard/docker-wrapper.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+URI="qdb://qdb-server:2836"
+
+if [ "${QDB_URI}" != "" ]
+then
+    echo "Using cluster uri: ${QDB_URI}"
+    URI=${QDB_URI}
+fi
+
+ALLOWED_ORIGIN="http://0.0.0.0:3449"
+if [ "${QDB_ALLOWED_ORIGIN}" != "" ]
+then
+    echo "Adding allowed origin: ${QDB_ALLOWED_ORIGIN}"
+    ALLOWED_ORIGIN=${QDB_ALLOWED_ORIGIN}
+fi
+
+cat /etc/qdb/qdb_rest.conf \
+    | jq ".allowed_origins = [\"${ALLOWED_ORIGIN}\"]" \
+    | jq ".assets = \"/var/lib/qdb/assets\"" \
+    | jq ".cluster_uri = \"${URI}\"" \
+    | jq ".tls_host = \"0.0.0.0\"" \
+    > /tmp/qdb_rest.conf.new && \
+    mv /tmp/qdb_rest.conf.new /etc/qdb/qdb_rest.conf && \
+    chown qdb:qdb /etc/qdb/qdb_rest.conf
+
+/usr/bin/qdb_rest --config-file /etc/qdb/qdb_rest.conf
