dbb8704263bc8d8c7621f40263990d518d26f1fc
Leon Mergen <leon@solatis.com>
2018-10-02 09:48:38 +0200

    Makes kafka-connect use the new codec layout

--- big-data-playground/qdb/docker-wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Launching qdbd..."
-qdbd --security=0 --address=eth0:2836
-
-# /usr/bin/env bash
---
+++ big-data-playground/run.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Pulling existing containers.."
+
+docker-compose pull
+
+echo "Building Kafka connector"
+
+cd kafka-connect-qdb && \
+    wget https://download.quasardb.net/quasardb/nightly/api/c/qdb-3.0.0master-linux-64bit-c-api.tar.gz && \
+    wget https://download.quasardb.net/quasardb/nightly/api/kafka/kafka-connect-qdb-3.0.0-SNAPSHOT-standalone.jar
+
+docker-compose build --no-cache
+
+echo "Launching QuasarDB"
+
+docker-compose up -d qdb-server
+
+echo "Waiting 3 seconds.."
+
+sleep 3
+
+echo "Creating 'test' table.."
+docker-compose run qdbsh -c "CREATE TABLE test(col1 INT64, col2 DOUBLE, col3 BLOB)"
+
+echo "Launching kafka"
+docker-compose up -d kafka
+
+echo "Waiting for kafka to come online.."
+sleep 10
+
+echo "Launching kafka-connect"
+docker-compose up -d kafka-connect-qdb
+
+echo "Sending 3 test rows to Kafka"
+
+echo '{"col1": 1234, "col2": 5.432, "col3": "hello, world!"}' | docker-compose exec kafka /opt/kafka/bin/kafka-console-producer.sh --topic test --broker-list kafka:9092
+echo '{"col1": 2345, "col2": 6.543, "col3": "hello, second world!"}' | docker-compose exec kafka /opt/kafka/bin/kafka-console-producer.sh --topic test --broker-list kafka:9092
+echo '{"col1": 3456, "col2": 7.654, "col3": "bye, world!"}' | docker-compose exec kafka /opt/kafka/bin/kafka-console-producer.sh --topic test --broker-list kafka:9092
+
+sleep 3
+
+echo "Showing data in QuasarDB:"
+docker-compose run qdbsh -c "SELECT * FROM test"
