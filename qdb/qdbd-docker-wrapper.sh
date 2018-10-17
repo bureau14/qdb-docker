75dfd5aae0a7b333dfaf7770981fc056de52cb25
Leon Mergen <leon@solatis.com>
2018-10-17 19:31:48 +0200

    Dynamically probe for bind IP address

--- qdb/qdbd-docker-wrapper.sh
+++ qdb/qdbd-docker-wrapper.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
 
+set -e
+set -x
+
 QDB_SERVER=`which qdbd`
 QDB_LAUNCH_ARGS=""
+IP=`which ip`
+AWK=`which awk`
 
-echo "Launching qdbd bound to eth0:2836"
-QDB_LAUNCH_ARGS="${QDB_LAUNCH_ARGS} -a eth0:2836"
+IP=`${IP} route get 8.8.8.8 | grep -oh 'src [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | awk '{print $2}'`
+
+echo "Launching qdbd bound to ${IP}:2836"
+QDB_LAUNCH_ARGS="${QDB_LAUNCH_ARGS} -a ${IP}:2836"
 
 # Detects the presence of a license file, and provides it as an
 # argument to `qdbd` if found.
