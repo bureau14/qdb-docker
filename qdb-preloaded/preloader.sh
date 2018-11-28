49ae013e49f47b89531ec41b3aed295130999c89
Camilo Polymeris <camilo@quasardb.net>
2018-11-28 19:30:33 -0300

    Update preloader to be compatible with newest railgun
    Also use a non-volume dir for qdb preloaded data

--- qdb-preloaded/preloader.sh
+++ qdb-preloaded/preloader.sh
@@ -6,7 +6,7 @@
 set -e
 
 touch ./qdbd.log                 # avoid tail complaining about a non-existing file
-qdbd -d --security=false -l .
+qdbd -d --security=false --root /data -l .
 echo "Waiting for qdbd..."
 grep -m 1 "successfully started quasardb daemon" <(tail -F ./qdbd.log)
 
@@ -14,9 +14,10 @@ for DATASET_DIR in `ls -d /datasets/*/`
 do
     echo "Loading $DATASET_DIR..."
 
-    qdb-railgun \
-        --delimiter , \
+    qdb_railgun \
         --with-header 1 \
         --config "$DATASET_DIR./config.json" \
         --file "$DATASET_DIR./data.csv"
 done
+
+grep -m 1 "stable" <(tail -F ./qdbd.log)
