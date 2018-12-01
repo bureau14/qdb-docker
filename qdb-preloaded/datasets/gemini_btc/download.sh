5e0fdd1c910a878069d25c67b6f436e98bc86510
Camilo Polymeris <camilo@quasardb.net>
2018-12-01 13:09:27 -0300

    Add BTCUSD dataset config

---
+++ qdb-preloaded/datasets/gemini_btc/download.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+wget https://www.cryptodatadownload.com/cdd/gemini_BTCUSD_2018_1min.csv
+wget https://www.cryptodatadownload.com/cdd/gemini_BTCUSD_2017_1min.csv
+wget https://www.cryptodatadownload.com/cdd/gemini_BTCUSD_2016_1min.csv
+wget https://www.cryptodatadownload.com/cdd/gemini_BTCUSD_2015_1min.csv
+
+# Combine in one file, removing headers
+echo "unix_time,timestamp,symbol,open,high,low,close,volume" > btcusd_1min.csv
+tail -n +3 gemini_BTCUSD_2018_1min.csv >> btcusd_1min.csv
+tail -n +3 gemini_BTCUSD_2017_1min.csv >> btcusd_1min.csv
+tail -n +3 gemini_BTCUSD_2016_1min.csv >> btcusd_1min.csv
+tail -n +3 gemini_BTCUSD_2015_1min.csv >> btcusd_1min.csv
+
+# Remove some columns
+cut -d, -f1,3 --complement btcusd_1min.csv > data.csv
+
+# Add a T to the timestamp column
+sed -i -re 's/([0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2})/\1T\2/' data.csv
