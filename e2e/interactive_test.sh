d81300ea3443bf407bdc9a885161056f620f4ba4
Camilo Polymeris <camilo@quasardb.net>
2019-01-08 10:46:03 -0300

    Document how to run e2e tests

--- e2e/interactive_test.sh
+++ e2e/interactive_test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-# First argument is the docker tag used for all the images tested (and tester)
+# First argument is the docker tag used for all the images tested
 TAG=$1
+shift
 
 # Setup SUT
 sut/start.sh ${TAG}
