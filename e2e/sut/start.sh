9d4f987e047700a25ecb488a1969db2257e44782
Camilo Polymeris <camilo@quasardb.net>
2019-01-09 08:36:52 -0300

    Check for tag argument in e2e/sut/start.sh

--- e2e/sut/start.sh
+++ e2e/sut/start.sh
@@ -3,6 +3,8 @@ set -e
 
 TAG=$1
 
+[ -z "$TAG" ] && echo "You must specify a docker tag" && exit 1
+
 echo "Starting SUT containers with tag $TAG..."
 
 docker run --name sut-qdb-preloaded --rm --network host --env QDB_DISABLE_SECURITY=true "bureau14/qdb-preloaded:$TAG" &
