cc8da1a67ca1d644829c0ee1bb870abd3f983dbb
Leon Mergen <leon@solatis.com>
2019-01-09 12:53:52 +0100

    Also try to execute download.sh if available

--- load-datasets.sh
+++ load-datasets.sh
@@ -6,19 +6,25 @@ set -o pipefail
 AWSCLI=`which aws`
 S3_BUCKET="qdbbuilddeps"
 S3_PATH="s3://${S3_BUCKET}/datasets"
-PATH="qdb-preloaded/datasets"
+DS_PATH="qdb-preloaded/datasets"
 
 function warn {
     echo $1
 }
 
-for DIR in ${PATH}/*/
+BASEDIR=$(pwd)
+
+for DIR in ${DS_PATH}/*/
 do
-    if [[ ${DIR} =~ (${PATH}/(.+)/$) ]]
+    if [[ ${DIR} =~ (${DS_PATH}/(.+)/$) ]]
     then
         IDENTIFIER=${BASH_REMATCH[2]}
-        ${AWSCLI} s3 cp ${S3_PATH}/${IDENTIFIER}.csv ${PATH}/${IDENTIFIER}/data.csv || warn "Dataset does not exist on S3: ${IDENTIFIER}"
+        ${AWSCLI} s3 cp ${S3_PATH}/${IDENTIFIER}.csv ${DS_PATH}/${IDENTIFIER}/data.csv || warn "Dataset does not exist on S3: ${IDENTIFIER}"
+        echo "current path: $(pwd)"
+        cd ${DS_PATH}/${IDENTIFIER}/ && ./download.sh || warn "No downloader exists: ${IDENTIFIER}"
+        cd ${BASEDIR}
     fi
+
 done
 
 
