8fd5a8a88099965384f2985af03cb0e0807c9dff
Leon Mergen <leon@solatis.com>
2019-01-09 09:23:38 +0100

    Splits up 'build' and 'push' phases

--- deployment.sh
+++ deployment.sh
@@ -9,6 +9,8 @@ source "container.sh"
 source "documentation.sh"
 source "files.sh"
 
+ACTION=$1
+
 get_versions qdb-dev-python
 
 detect_version
@@ -57,19 +59,31 @@ echo "Number of container: ${#CONTAINERS_NAMES[@]}"
 echo "------------------"
 mkdir -p build
 cd build
-if [[ ${#CONTAINERS_NAMES[@]} != ${#CONTAINERS_FILES[@]} ]]; then
+if [[ ${#CONTAINERS_NAMES[@]} != ${#CONTAINERS_FILES[@]} ]]
+then
     echo "container names: ${#CONTAINERS_NAMES[@]}"
     echo "container files: ${#CONTAINERS_FILES[@]}"
     echo "Wrong number of names or files bundle. Aborting..."
-    exit -1
+    exit 1
 fi
 
-for ((index=0; index < (${#CONTAINERS_NAMES[@]} +1) ; index++)); do
-    if [[ ! -z "${CONTAINERS_NAMES[$index]}" ]]; then
-        print_info_container ${CONTAINERS_NAMES[$index]} ${CONTAINERS_FILES[$index]}
-        build_container ${CONTAINERS_NAMES[$index]} ${CONTAINERS_FILES[$index]}
-        push_container ${CONTAINERS_NAMES[$index]}
-        update_documentation ${CONTAINERS_NAMES[$index]} ${CONTAINERS_FILES[$index]}
+for ((index=0; index < (${#CONTAINERS_NAMES[@]} +1) ; index++))
+do
+    if [[ ! -z "${CONTAINERS_NAMES[$index]}" ]]
+    then
+        if [[ "${ACTION}" == "build" ]]
+        then
+            print_info_container ${CONTAINERS_NAMES[$index]} ${CONTAINERS_FILES[$index]}
+            build_container ${CONTAINERS_NAMES[$index]} ${CONTAINERS_FILES[$index]}
+        elif [[ "${ACTION}" == "push" ]]
+        then
+            push_container ${CONTAINERS_NAMES[$index]}
+            update_documentation ${CONTAINERS_NAMES[$index]} ${CONTAINERS_FILES[$index]}
+        else
+            echo "Invalid action: ${ACTION}"
+            echo "Needs to be either 'build' or 'push'"
+            exit 1
+        fi
         echo "------------------"
     fi
 done
