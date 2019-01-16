e443fbcac0eb5aa14ddf292efbc1525e4254c012
Leon Mergen <leon@solatis.com>
2019-01-16 13:06:33 +0100

    Also accept --tags as comma-separated list

--- deployment.sh
+++ deployment.sh
@@ -24,6 +24,12 @@ do
             shift # past argument
             shift # past value
             ;;
+        --tags)
+            xs=(${2//,/ })
+            TAGS=("${TAGS[@]}" "${xs[@]}")
+            shift # past argument
+            shift # past value
+            ;;
         *)
             POSITIONAL+=("$1") # save it in an array for later
             shift # past argument
