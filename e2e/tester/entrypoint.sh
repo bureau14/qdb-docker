4a076d5ee1caff456bdabfccac9624afb0f8f251
Camilo Polymeris <camilo@quasardb.net>
2019-01-16 11:27:27 -0300

    e2e: Log wait-on output

--- e2e/tester/entrypoint.sh
+++ e2e/tester/entrypoint.sh
@@ -8,6 +8,6 @@ CYPRESS=node_modules/cypress/bin/cypress
 DASHBOARD_URI=http://sut-qdb-dashboard:40000
 
 echo "Waiting for dashboard at $DASHBOARD_URI"
-${WAIT_ON} -t 10000 ${DASHBOARD_URI}
+${WAIT_ON} --log -t 10000 ${DASHBOARD_URI}
 ${CYPRESS} run $@
 chmod -R a+rw ./cypress/reports ./cypress/screenshots ./cypress/videos
