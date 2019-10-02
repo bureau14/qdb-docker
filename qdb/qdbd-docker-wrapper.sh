261de222d2dacb35b99bf934b6f0c32d3c5194f8
Leon Mergen <leon@solatis.com>
2019-10-02 10:08:37 +0200

    This didn't work well anyway

--- qdb/qdbd-docker-wrapper.sh
+++ qdb/qdbd-docker-wrapper.sh
@@ -13,15 +13,6 @@ IP=`${IP} route get 8.8.8.8 | grep -oh 'src [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' 
 echo "Launching qdbd bound to ${IP}:2836"
 QDB_LAUNCH_ARGS="${QDB_LAUNCH_ARGS} -a ${IP}:2836"
 
-# Detects the presence of a license file, and provides it as an
-# argument to `qdbd` if found.
-POTENTIAL_LICENSE_FILE="/var/lib/qdb/license.txt"
-
-if [ -f ${POTENTIAL_LICENSE_FILE} ]; then
-    echo "license file found! ${POTENTIAL_LICENSE_FILE}"
-    QDB_LAUNCH_ARGS="${QDB_LAUNCH_ARGS} --license-file ${POTENTIAL_LICENSE_FILE}"
-fi
-
 if [ "${QDB_DISABLE_SECURITY}" = "true" ]
 then
     QDB_LAUNCH_ARGS="${QDB_LAUNCH_ARGS} --security=0"
