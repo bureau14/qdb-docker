ceb2d1402335f96ac342888d6c950c375e5a72cb
Leon Mergen <leon@solatis.com>
2019-10-08 13:22:13 +0200

    Various improvements to security and usability of docker container

--- qdb/qdbd-docker-wrapper.sh
+++ qdb/qdbd-docker-wrapper.sh
@@ -1,27 +1,68 @@
 #!/usr/bin/env sh
 
 set -e
-set -x
 
-QDB_CONFIG=${QDB_CONFIG_PATH:-"/opt/qdb/etc/qdbd.conf"}
-QDB_SERVER="/opt/qdb/bin/qdbd"
+QDB_CONFIG="/tmp/qdbd.conf"
+cp -v ${QDB_CONFIG_PATH} ${QDB_CONFIG}
+
+QDB_SERVER="/home/lmergen/.local/bin/qdbd"
 QDB_LAUNCH_ARGS=""
 IP=`which ip`
 AWK=`which awk`
+MKTEMP=`which mktemp`
+JQ=`which jq`
 
 IP=`${IP} route get 8.8.8.8 | grep -oh 'src [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | awk '{print $2}'`
 
 echo "Launching qdbd bound to ${IP}:2836"
 QDB_LAUNCH_ARGS="${QDB_LAUNCH_ARGS} -a ${IP}:2836"
 
-if [ "${QDB_DISABLE_SECURITY}" = "true" ]
-then
-    QDB_LAUNCH_ARGS="${QDB_LAUNCH_ARGS} --security=0"
-fi
+function die {
+    echo "" >> /dev/stderr
+    echo "******************"  >> /dev/stderr
+    echo $1 >> /dev/stderr
+    echo "******************"  >> /dev/stderr
+    exit 1
+}
 
-if [ "${QDB_CONFIG_PATH}" != "" ]
+function patch_conf {
+    KEY=$1
+    VALUE=$2
+
+    F=$(${MKTEMP})
+    cat ${QDB_CONFIG} | ${JQ} -r "${KEY} |= ${VALUE}" > ${F}
+    mv ${F} ${QDB_CONFIG}
+}
+
+function file_or_string {
+    MAYBE_STRING=$1
+    MAYBE_FILE=$2
+
+    F=$(${MKTEMP})
+
+    if [[ ! -z ${!MAYBE_STRING} ]]
+    then
+        echo ${!MAYBE_STRING} > ${F}
+    elif [[ ! -z ${!MAYBE_FILE} ]]
+    then
+        F=${!MAYBE_FILE}
+    else
+        die "Neither ${MAYBE_STRING} nor ${MAYBE_FILE} is set!"
+    fi
+
+    echo ${F}
+}
+
+if [ "${QDB_ENABLE_SECURITY}" = "true" ]
 then
-    QDB_LAUNCH_ARGS="${QDB_LAUNCH_ARGS} --config=${QDB_CONFIG_PATH}"
+    echo "Enabling security"
+    PRIVKEY=$(file_or_string "QDB_CLUSTER_PRIVATE_KEY" "QDB_CLUSTER_PRIVATE_KEY_FILE")
+    ULIST=$(file_or_string "QDB_USER_LIST" "QDB_USER_LIST_FILE")
+
+    patch_conf ".global.security.enabled" "true"
+    patch_conf ".global.security.encrypt_traffic" "true"
+    patch_conf ".global.security.cluster_private_file" "\"${PRIVKEY}\""
+    patch_conf ".global.security.user_list" "\"${ULIST}\""
 fi
 
 echo "Launching qdb with arguments: ${QDB_LAUNCH_ARGS}"
