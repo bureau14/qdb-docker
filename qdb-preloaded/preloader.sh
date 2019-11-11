a67af1c1b7ca986ace71421a22dcd856e600d0c1
Leon Mergen <leon@solatis.com>
2019-11-11 10:14:52 +0100

    Fix qdb-preloaded docker image due to more restrictive qdb image permissions

--- qdb-preloaded/preloader.sh
+++ qdb-preloaded/preloader.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 # This script is ran during qdb-preloaded's build-time
-# It will use railgun to load data from each subdirectory of /datasets
+# It will use railgun to load data from each subdirectory of /opt/qdb/datasets
 # Each of this subdirectories should include a data.csv and a railgun-compatible config.json
 
 set -e
 
 touch ./qdbd.log                 # avoid tail complaining about a non-existing file
-/opt/qdb/bin/qdbd -d --security=false --rocksdb-root /data -l .
+
+/opt/qdb/bin/qdbd -d --security=false --rocksdb-root /opt/qdb/db -l .
 echo "Waiting for qdbd..."
 grep -m 1 "successfully started quasardb daemon" <(tail -F ./qdbd.log)
 
-for DATASET_DIR in `ls -d /datasets/*/`
+for DATASET_DIR in `ls -d /opt/qdb/datasets/*/`
 do
     if [[ -f ${DATASET_DIR}/config.json && -f ${DATASET_DIR}/data.csv ]]
     then
