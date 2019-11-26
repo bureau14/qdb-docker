0490bbd795dbe8b42e07cc060c47dc70173fdfd9
Leon Mergen <leon@solatis.com>
2019-11-26 10:17:38 +0100

    Slightly optimize script to better deal with no containers to stop

--- scripts/stop-all-containers.sh
+++ scripts/stop-all-containers.sh
@@ -2,4 +2,10 @@
 
 set -ex
 
-docker ps | tail -n +2 | awk '{print $1}' | xargs docker rm -f
+CONTAINERS=$(docker ps | tail -n +2 | awk '{print $1}')
+
+for CONTAINER in ${CONTAINERS}
+do
+    echo "Removing container: ${CONTAINER}"
+    docker rm -f ${CONTAINER}
+done
