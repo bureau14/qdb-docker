3b64841ec3e7369e342c5f87e569aeba174f2cf6
terngkub <terngnattapol@gmail.com>
2020-04-09 10:24:07 +0200

    Bump version to 3.8.0.

--- deployment.sh
+++ deployment.sh
@@ -13,7 +13,7 @@ source "files.sh"
 #   https://stackoverflow.com/a/14203146
 ##
 POSITIONAL=()
-TAGS=("3" "3.8" "3.8.0" "nightly")
+TAGS=("3" "3.8" "3.8.0" "latest")
 while [[ $# -gt 0 ]]
 do
     key="$1"
