c032d8d0e4caa888b89908616ac0b45fbad8dd87
terngkub <terngnattapol@gmail.com>
2020-08-03 18:53:03 +0200

    Bump version to 3.9.0.

--- deployment.sh
+++ deployment.sh
@@ -13,7 +13,7 @@ source "files.sh"
 #   https://stackoverflow.com/a/14203146
 ##
 POSITIONAL=()
-TAGS=("3" "3.9" "3.9.0" "nightly")
+TAGS=("3.9" "3.9.0" "3" "latest" "stable")
 while [[ $# -gt 0 ]]
 do
     key="$1"
