5bb0267b122eec5e8a2a000eb52221f02c181c0f
terngkub <terngnattapol@gmail.com>
2020-08-06 11:51:00 +0200

    Fix double quotes error in e2e query

--- e2e/tester/cypress/integration/query-table.spec.js
+++ e2e/tester/cypress/integration/query-table.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 
-const btcusd_query = "select $timestamp, close, high, low, open, volume from fx.btcusd in range(2016-01-09T13:55:00Z, 2016-01-09T16:00:00Z);";
+const btcusd_query = "select $timestamp, close, high, low, open, volume from \"fx.btcusd\" in range(2016-01-09T13:55:00Z, 2016-01-09T16:00:00Z);";
 
 context("Performing btcusd Query", () => {
   before(() => {
