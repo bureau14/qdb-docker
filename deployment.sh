897f987b64f187c21297c12fb3cd59256fb0a290
Leon Mergen <leon@solatis.com>
2021-09-29 12:02:59 +0200

    Bump version to 3.10.0.

--- deployment.sh
+++ deployment.sh
@@ -13,7 +13,7 @@ source "files.sh"
 #   https://stackoverflow.com/a/14203146
 ##
 POSITIONAL=()
-TAGS=("nightly")
+TAGS=("3.10" "3.10.0" "3" "latest" "stable")
 while [[ $# -gt 0 ]]
 do
     key="$1"
