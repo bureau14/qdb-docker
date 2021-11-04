34b291e2c811396d267a5d99c6412604d7734519
Leon Mergen <leon@solatis.com>
2021-11-04 16:06:01 +0100

    Add kinesis connector to build scripts

--- files.sh
+++ files.sh
@@ -20,4 +20,5 @@ function set_files {
     TARBALL_QDB_API=$(find_file 'qdb-*-linux-64bit-c-api.tar.gz')
     TARBALL_QDB_REST=$(find_file 'qdb-*-linux-64bit-rest.tar.gz')
     TARBALL_QDB_UTILS=$(find_file 'qdb-*-linux-64bit-utils.tar.gz')
+    JAR_QDB_KINESIS_CONNECTOR=$(find_file 'kinesis-*-jar-with-dependencies.jar')
 }
