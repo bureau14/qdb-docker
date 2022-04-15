9f4a6b9f188cbeca3e030e26765e991b778fc874
Leon Mergen <leon@solatis.com>
2022-04-15 16:59:17 +0200

    Horey sheet we have an s3 compatible docker image

--- qdb/qdbd-docker-wrapper.sh
+++ qdb/qdbd-docker-wrapper.sh
@@ -95,6 +95,28 @@ function bootstrap_peers {
     echo ${RET}
 }
 
+# Usability improvement: first verify we can write to the depot path
+
+if [ ! -w "${QDB_DEPOT_PATH}" ]
+then
+    echo ""
+    echo "**********************************************************************"
+    echo "Error: path ${QDB_DEPOT_PATH} is not writable to the current user."
+    echo ""
+    echo "If you are mounting this path externally, please ensure it's writable "
+    echo "for user ${USER} with uid ${UID}"
+    echo "**********************************************************************"
+    echo ""
+    echo "Troubleshooting information (stat output): "
+    echo ""
+    stat ${QDB_DEPOT_PATH} || true
+    exit 1
+fi
+
+###
+# QuasarDB
+###
+
 if [ "${QDB_ENABLE_SECURITY}" = "true" ]
 then
     echo "Enabling security"
@@ -112,6 +134,11 @@ then
     QDB_LAUNCH_ARGS="${QDB_LAUNCH_ARGS} --advertised-address ${QDB_ADVERTISED_ADDRESS}"
 fi
 
+if [[ ! -z ${QDB_REPLICATION} ]]
+then
+    echo "Enabling QuasarDB replication factor ${QDB_REPLICATION}"
+    patch_conf ".global.cluster.replication_factor" "${QDB_REPLICATION}"
+fi
 
 if [[ ! -z ${QDB_LICENSE} ]]
 then
@@ -121,13 +148,6 @@ elif [[ ! -z ${QDB_LICENSE_FILE} ]]
 then
     echo "Enabling license file: ${QDB_LICENSE_FILE}"
     patch_conf ".local.user.license_file" "\"${QDB_LICENSE_FILE}\""
-fi
-
-
-if [[ ! -z ${QDB_REPLICATION} ]]
-then
-    echo "Enabling QuasarDB replication factor ${QDB_REPLICATION}"
-    patch_conf ".global.cluster.replication_factor" "${QDB_REPLICATION}"
 fi
 
 if [[ ! -z ${QDB_MEMORY_LIMIT_SOFT} ]]
@@ -142,16 +162,136 @@ then
     patch_conf ".local.limiter.max_bytes_hard" "${QDB_MEMORY_LIMIT_HARD}"
 fi
 
-if [[ ! -z ${QDB_COLUMN_FAMILY_OPTIONS} ]]
+
+
+###
+# Rocksdb
+###
+
+if [[ ! -z ${QDB_DEPOT_PATH} ]]
 then
-    echo "Setting rocksdb column family options to '${QDB_COLUMN_FAMILY_OPTIONS}'"
-    patch_conf ".local.depot.rocksdb.column_family_options" "\"${QDB_COLUMN_FAMILY_OPTIONS}\""
+    echo "Setting rocksdb depot path to '${QDB_DEPOT_PATH}'"
+    patch_conf ".local.depot.rocksdb.root" "\"${QDB_DEPOT_PATH}\""
 fi
 
-if [[ ! -z ${QDB_SST_PARTITIONER_THRESHOLD} ]]
+if [[ ! -z ${QDB_ROCKSDB_COLUMN_FAMILY_OPTIONS} ]]
 then
-    echo "Setting rocksdb SST partitioner threshold to ${QDB_SST_PARTITIONER_THRESHOLD}"
-    patch_conf ".local.depot.rocksdb.sst_partitionner_threshold" "${QDB_SST_PARTITIONER_THRESHOLD}"
+    echo "Setting rocksdb column family options to '${QDB_ROCKSDB_COLUMN_FAMILY_OPTIONS}'"
+    patch_conf ".local.depot.rocksdb.column_family_options" "\"${QDB_ROCKSDB_COLUMN_FAMILY_OPTIONS}\""
+fi
+
+if [[ ! -z ${QDB_ROCKSDB_SST_PARTITIONER_THRESHOLD} ]]
+then
+    echo "Setting rocksdb SST partitioner threshold to ${QDB_ROCKSDB_SST_PARTITIONER_THRESHOLD}"
+    patch_conf ".local.depot.rocksdb.sst_partitionner_threshold" "${QDB_ROCKSDB_SST_PARTITIONER_THRESHOLD}"
+fi
+
+if [[ ! -z ${QDB_ROCKSDB_THREADS} ]]
+then
+    echo "Setting rocksdb regular (compaction) threads to ${QDB_ROCKSDB_THREADS}"
+    patch_conf ".local.depot.rocksdb.threads" "${QDB_ROCKSDB_THREADS}"
+fi
+
+if [[ ! -z ${QDB_ROCKSDB_HI_THREADS} ]]
+then
+    echo "Setting rocksdb hi (flush) threads to ${QDB_ROCKSDB_HI_THREADS}"
+    patch_conf ".local.depot.rocksdb.hi_threads" "${QDB_ROCKSDB_HI_THREADS}"
+fi
+
+###
+# Rocksdb cloud
+###
+#
+# Identity / provider / location configurations
+#
+###
+
+if [[ ! -z ${QDB_CLOUD_PROVIDER} ]]
+then
+    echo "Setting cloud provider ${QDB_CLOUD_PROVIDER}"
+    patch_conf ".local.depot.rocksdb.cloud.provider" "\"${QDB_CLOUD_PROVIDER}\""
+fi
+
+if [ "${QDB_CLOUD_AWS_ENABLE_INSTANCE_AUTH}" = "true" ]
+then
+    echo "Enabling instance-based credential discovery"
+    patch_conf ".local.depot.rocksdb.cloud.aws.use_instance_auth" "true"
+else
+    patch_conf ".local.depot.rocksdb.cloud.aws.use_instance_auth" "false"
+
+    # Not secret
+    if [[ ! -z ${QDB_CLOUD_AWS_ACCESS_KEY_ID} ]]
+    then
+        echo "Setting cloud aws access key id to ${QDB_CLOUD_AWS_ACCESS_KEY_ID}"
+        patch_conf ".local.depot.rocksdb.cloud.aws.access_key_id" "\"${QDB_CLOUD_AWS_ACCESS_KEY_ID}\""
+    fi
+
+    # SECRET, DO *NOT* LOG!
+    if [[ ! -z ${QDB_CLOUD_AWS_SECRET_ACCESS_KEY} ]]
+    then
+        echo "Setting cloud aws secret access key. "
+        echo ""
+        echo "**********************************************************************"
+        echo "Warning: secrets as environment variables is considered insecure. "
+        echo "We recommend using instance-based authentication."
+        echo "**********************************************************************"
+        echo ""
+        patch_conf ".local.depot.rocksdb.cloud.aws.secret_key" "\"${QDB_CLOUD_AWS_SECRET_ACCESS_KEY}\""
+    fi
+fi
+
+if [[ ! -z ${QDB_CLOUD_BUCKET_REGION} ]]
+then
+    echo "Setting s3 bucket region to ${QDB_CLOUD_BUCKET_REGION}"
+    patch_conf ".local.depot.rocksdb.cloud.bucket.region" "\"${QDB_CLOUD_BUCKET_REGION}\""
+fi
+
+if [[ ! -z ${QDB_CLOUD_BUCKET_PREFIX} ]]
+then
+    echo "Setting s3 bucket prefix to ${QDB_CLOUD_BUCKET_PREFIX}"
+    patch_conf ".local.depot.rocksdb.cloud.bucket.source_prefix" "\"${QDB_CLOUD_BUCKET_PREFIX}\""
+    patch_conf ".local.depot.rocksdb.cloud.bucket.destination_prefix" "\"${QDB_CLOUD_BUCKET_PREFIX}\""
+fi
+
+if [[ ! -z ${QDB_CLOUD_BUCKET_SUFFIX} ]]
+then
+    echo "Setting s3 bucket suffix to ${QDB_CLOUD_BUCKET_SUFFIX}"
+    patch_conf ".local.depot.rocksdb.cloud.bucket.source_suffix" "\"${QDB_CLOUD_BUCKET_SUFFIX}\""
+    patch_conf ".local.depot.rocksdb.cloud.bucket.destination_suffix" "\"${QDB_CLOUD_BUCKET_SUFFIX}\""
+fi
+
+if [[ ! -z ${QDB_CLOUD_BUCKET_PATH_PREFIX} ]]
+then
+    echo "Setting s3 bucket path to ${QDB_CLOUD_BUCKET_PATH_PREFIX}"
+    patch_conf ".local.depot.rocksdb.cloud.bucket.path_prefix" "\"${QDB_CLOUD_BUCKET_PATH_PREFIX}\""
+fi
+
+###
+# Rocksdb cloud
+###
+#
+# Performance-related configurations
+#
+###
+
+if [[ ! -z ${QDB_CLOUD_LOCAL_SST_CACHE} ]]
+then
+    echo "Setting cloud local cache to ${QDB_CLOUD_LOCAL_SST_CACHE} bytes"
+    patch_conf ".local.depot.rocksdb.cloud.local_sst_cache_size" "${QDB_CLOUD_LOCAL_SST_CACHE_SIZE}"
+fi
+
+if [[ ! -z ${QDB_CLOUD_AWS_TRANSFER_MANAGER_THREADS} ]]
+then
+    echo "Setting AWS transfer manager thread count to ${QDB_CLOUD_AWS_TRANSFER_MANAGER_THREADS}"
+    patch_conf ".local.depot.rocksdb.cloud.aws.use_transfer_manager" "true"
+    patch_conf ".local.depot.rocksdb.cloud.aws.transfer_manager_threads" "${QDB_CLOUD_AWS_TRANSFER_MANAGER_THREADS}"
+fi
+
+if [[ ! -z ${QDB_CLOUD_AWS_TRANSFER_MANAGER_BUFFER_SIZE} ]]
+then
+    echo "Setting AWS transfer manager buffer size to ${QDB_CLOUD_AWS_TRANSFER_MANAGER_BUFFER_SIZE}"
+    patch_conf ".local.depot.rocksdb.cloud.aws.use_transfer_manager" "true"
+    patch_conf ".local.depot.rocksdb.cloud.aws.transfer_manager_buffer_size" "${QDB_CLOUD_AWS_TRANSFER_MANAGER_BUFFER_SIZE}"
 fi
 
 if [[ ! -z ${K8S_REPLICA_COUNT} ]]
