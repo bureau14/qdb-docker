8a0861197fecd586a2b7136a0f3c3e6d94bff75f
Leon Mergen <leon@solatis.com>
2022-06-10 13:33:01 +0200

    Fix some integration issues

--- 3.10/retry.sh
+++ 3.10/retry.sh
@@ -2,9 +2,12 @@
 TIMES=$1
 shift;
 
-while [[ $TIMES -ne 0 ]] && ! $@
+COMMAND="/bin/bash -c '$@'"
+
+while [[ $TIMES -ne 0 ]] && ! /usr/bin/env bash -c "${COMMAND[@]}"
 do
     TIMES=$((TIMES-1))
+    sleep 1
 done
 
 if [[ $TIMES -eq 0 ]]
--- templates/retry.sh
+++ templates/retry.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+
 TIMES=$1
 shift;
 
-while [[ $TIMES -ne 0 ]] && ! $@
+COMMAND="/bin/bash -c '$@'"
+
+while [[ $TIMES -ne 0 ]] && ! /usr/bin/env bash -c "${COMMAND[@]}"
 do
     TIMES=$((TIMES-1))
+    sleep 1
 done
 
 if [[ $TIMES -eq 0 ]]
